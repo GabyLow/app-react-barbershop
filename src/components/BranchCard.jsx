64a5cd28ca6260bd19cac2fde4df6748ea04ce69
GabyLow <127358083+GabyLow@users.noreply.github.com>
2023-09-02 16:32:09 -0600

    :sparkles:feat(add):info in branchpage

---
+++ src/components/BranchCard.jsx
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+const BranchCard = ({ imagenSrc, direccion, horarios, telefono, mapaSrc }) => {
+  return (
+    <div className="max-w-md rounded overflow-hidden shadow-lg mx-4 my-4">
+      <img src={imagenSrc} alt="Imagen de la sucursal" className="w-300 h-300" />
+      <div className="px-6 py-4">
+        <h2 className="font-semibold text-orange-200  text-xl mb-2">Dirección:</h2>
+        <p className="text-white">{direccion}</p>
+        <h2 className="font-semibold text-orange-200  text-xl mt-4 mb-2">Horarios:</h2>
+        <p className="text-white">{horarios}</p>
+        <h2 className="font-semibold text-orange-200  text-xl mt-4 mb-2">Teléfono:</h2>
+        <p className="text-white">{telefono}</p>
+      </div>
+      <div className="px-6 py-4">
+        <iframe
+          title="Mapa de la sucursal"
+          src={mapaSrc}
+          width="100%"
+          height="300"
+          style={{ border: '0' }}
+          allowFullScreen=""
+          aria-hidden="false"
+          tabIndex="0"
+        ></iframe>
+      </div>
+    </div>
+  );
+};
+
+BranchCard.propTypes = {
+    imagenSrc: PropTypes.string.isRequired, // Asegura que imagenSrc sea una cadena (string) y es requerido
+    direccion: PropTypes.string.isRequired,
+    horarios: PropTypes.string.isRequired,
+    telefono: PropTypes.string.isRequired,
+    mapaSrc: PropTypes.string.isRequired,
+  };
+
+export default BranchCard;
--- src/pages/BranchPage.jsx
+++ src/pages/BranchPage.jsx
@@ -1,13 +1,37 @@
-import HomeCard from "../components/HomeCard";
 
-const BranchPage = () => {
+import BranchCard from '../components/BranchCard';
+
+function BranchPage() {
   return (
-    <>
-      <div className="container mx-auto mt-8">
-        <HomeCard/>
+    <div className="bg-zinc-950/80 min-h-screen flex justify-center items-center">
+      <div className="flex flex-wrap justify-center">
+        <div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2 p-4">
+          <BranchCard
+            imagenSrc="https://images.unsplash.com/photo-1585747860715-2ba37e788b70?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2074&q=80"
+            direccion="Boulevard del Hipodromo #1204, Col. San Benito, San Salvador, El salvador."
+            horarios="Lunes a Sabado de 08:00 a.m. a 06:00 p.m., sin cerrar al mediodía."
+            telefono="(503) 2522-9999"
+            mapaSrc="URL_MAPA_SUCURSAL_1"
+          />
+        </div>
+        <div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2 p-4">
+          <BranchCard
+            imagenSrc="https://media.quincemil.com/imagenes/2022/05/28012746/Peluqueria_M%C3%A1gica-8-1706x960.jpg"
+            direccion="Boulevard Santa Elena #2521, Urb. Santa Elena, Antiguo Cuscatlan, La Libertad, El Salvador."
+            horarios="Lunes a Sabado de 08:00 a.m. a 06:00 p.m., sin cerrar al mediodía."
+            telefono="(503) 2522-5055"
+            mapaSrc="URL_MAPA_SUCURSAL_2"
+          />
+        </div>
       </div>
-    </>
+    </div>
   );
-};
+}
 
 export default BranchPage;
+
+
+
+
+
+
