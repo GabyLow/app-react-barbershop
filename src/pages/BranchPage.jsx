388a81fa66e7172823576368bb30a31a5cd396f5
GabyLow <127358083+GabyLow@users.noreply.github.com>
2023-09-02 20:08:17 -0600

    :hammer:(fix) Problem with branch page images

--- src/pages/BranchPage.jsx
+++ src/pages/BranchPage.jsx
@@ -7,7 +7,7 @@ function BranchPage() {
       <div className="flex flex-wrap justify-center">
         <div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2 p-4">
           <BranchCard
-            imagenSrc="/public/Barber-branch-1.png"
+            imagenSrc="/Barber-branch-1.png"
             direccion="Boulevard del Hipodromo #1204, Col. San Benito, San Salvador, El salvador."
             horarios="Lunes a Sabado de 08:00 a.m. a 06:00 p.m., sin cerrar al mediodía."
             telefono="(503) 2522-9999"
@@ -16,7 +16,7 @@ function BranchPage() {
         </div>
         <div className="w-full md:w-1/2 lg:w-1/2 xl:w-1/2 p-4">
           <BranchCard
-            imagenSrc="/public/Barber-branch-2.png"
+            imagenSrc="/Barber-branch-2.png"
             direccion="Boulevard Santa Elena Local #12, Plaza Madero, Antiguo Cuscatlan, La Libertad, El Salvador."
             horarios="Lunes a Sabado de 08:00 a.m. a 06:00 p.m., sin cerrar al mediodía."
             telefono="(503) 2522-5055"
